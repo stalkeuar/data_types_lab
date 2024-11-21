f7acaac2a7c379aca16d0190be454023ecf55a2b
stalkeuar <mai888sutalkuar@gmail.com>
2024-11-21 12:09:34 +0000

    1

---
+++ task4.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+	int parametr;
+	float temp;
+
+	std::cout << "Оберіть параметр (1 - Ц в Ф, 2 - Ф в Ц): ";
+	std::cin >> parametr;
+
+	if (parametr > 2) {
+                std::cout << "Помилка! невірний параметр. " << std::endl;
+                return 0;
+        }
+	else if (parametr == 1, 2) {	
+		std::cout << "Введіть температуру: ";
+	}
+
+	std::cin >> temp;
+
+	if (temp < -273.15) {
+		std::cout << "Занадто холодно для підрахунку!" << std::endl;
+		return 0;
+	}
+
+
+	if (parametr == 1) {
+		std::cout << "Температура у фаренгейтах: " << ((temp * 9) / 5) + 32 << std::endl;
+	} 
+	else if (parametr == 2) {
+		std::cout << "Температура в цельсіях: " << ((temp - 32) * 5) / 9 << std::endl;
+	}
+	else {
+		std::cout << "Помилка! невірний параметр. " << std::endl;
+		return 0;
+	}
+
+	return 0;
+}
